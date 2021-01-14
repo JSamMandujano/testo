45090e26dad82ca33cdbb935f76f0f80324391ed
Mandujano Lopez <jose.mandujano.lopez@accenture.com>
2021-01-13 18:59:36 -0600

    update

--- src/components/Interview.js
+++ src/components/Interview.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel'
 import Form from 'react-bootstrap/Form'
 import './interview.css';
@@ -23,14 +23,10 @@ import {
 
 export const Interview = () => {
 
-  const history = useHistory();
-  const {candidate} = useContext(CandidateContext) //cuando estan en el mismo nivel los componentes e hijo a padre
-console.log(candidate);
-
   const questions = [
     {
         id: 1,
-        subject: 'Javascript',
+        subject:"Javascript",
         question: '¿Cuáles son las posibles formas de crear objetos en JavaScript?',
         answerOptions: [
           { answerText: 'Incorrect', isCorrect: false },
@@ -40,7 +36,7 @@ console.log(candidate);
     },
     {
         id: 2,
-        subject: 'CSS',
+        subject:"CSS",
         question: '¿Cuál es la diferencia entre padding y border?',
         answerOptions: [
           { answerText: 'Incorrect', isCorrect: false },
@@ -50,7 +46,7 @@ console.log(candidate);
     },
     {
         id: 3,
-        subject: 'React',
+        subject:"React",
         question: '¿Cuál es la diferencia entre context api y redux?',
         answerOptions: [
           { answerText: 'Incorrect', isCorrect: false },
@@ -71,12 +67,48 @@ console.log(candidate);
   }
 ]
 
+  const history = useHistory();
+  const {candidate} = useContext(CandidateContext) //cuando estan en el mismo nivel los componentes e hijo a padre
+console.log(candidate);
+
+const [filteredQuestions, setFilteredQuestions] = useState([]);
+
+useEffect(() => {
+  if(candidate) {
+  questionsFiltering();
+  }
+  }, [candidate])
+
+const questionsFiltering = () => {  
+  console.log(questions);
+  const newQuestionsArray = questions.map((question) => {
+  if(candidate.htmlskills === "on" && question.subject === "HTML") {
+  return question;
+  }
+  if (candidate.javascriptskills === "on" && question.subject === "Javascript") {
+  return question;
+  }
+  if (candidate.cssskills === "on" && question.subject === "CSS") {
+  return question;
+  }
+  if (candidate.reactskills === "on" && question.subject === "React") {
+  return question;
+  }
+});
+console.log(newQuestionsArray);
+const newFilterQuestions = newQuestionsArray.filter(question => question != undefined);
+
+  setFilteredQuestions(newFilterQuestions);
+  };
+
+console.log(filteredQuestions);
+
   return (
     <div>
     <Navbar color="dark" dark> </Navbar>
              <h1>Questions</h1>
     <Carousel interval={1000000}>
-              {questions.map(question => (
+              {filteredQuestions.map(filteredQuestion => (
 
   <Carousel.Item>
     <img
@@ -86,8 +118,8 @@ console.log(candidate);
     />
     
     <Carousel.Caption>
-      <h3>{question.subject}</h3>
-      <p>{question.question}</p>
+      <h3>{filteredQuestion.subject}</h3>
+      <p>{filteredQuestion.question}</p>
       <Form.Check className="moveradio" type="radio" inline label="Correcto" aria-label="radio 1" />
       <Form.Check className="moveradiu" type="radio" inline label="Incorrecto" aria-label="radio 1" />
       
